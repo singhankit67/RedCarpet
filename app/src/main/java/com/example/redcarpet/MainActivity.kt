189ba340ae2e97eb5ead2d8b53e8c95ad468b029
Ankit Singh <55789523+singhankit67@users.noreply.github.com>
2020-11-10 02:28:58 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/redcarpet/MainActivity.kt
+++ app/src/main/java/com/example/redcarpet/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dagger.hilt.android.AndroidEntryPoint
 
+//If you annotate an Android class with @AndroidEntryPoint, then you also must annotate Android classes that depend on it. For example, if you annotate a fragment,
+//then you must also annotate any activities where you use that fragment.
+
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
