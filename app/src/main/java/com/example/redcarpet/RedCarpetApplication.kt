34bae8e4737f1363dd5bc62fda305aaacc6c7eb8
Ankit Singh <55789523+singhankit67@users.noreply.github.com>
2020-11-10 02:32:27 +0530

    Update RedCarpetApplication.kt

--- app/src/main/java/com/example/redcarpet/RedCarpetApplication.kt
+++ app/src/main/java/com/example/redcarpet/RedCarpetApplication.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import android.os.Build
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+
+//RedCarpetApplication class: This class is necessary for the hilt and you should annotate it with @HiltAndroidApp. Don't forget to add it to the manifest file in the application tag.
 @HiltAndroidApp
 class RedCarpetApplication:Application() {
     override fun onCreate() {
