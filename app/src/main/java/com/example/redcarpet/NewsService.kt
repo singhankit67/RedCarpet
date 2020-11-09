4259e0c2e59d37b136af471d78832d237c30d101
Ankit Singh <55789523+singhankit67@users.noreply.github.com>
2020-11-10 02:05:00 +0530

    Update NewsService.kt

--- app/src/main/java/com/example/redcarpet/NewsService.kt
+++ app/src/main/java/com/example/redcarpet/NewsService.kt
@@ -7,3 +7,4 @@ interface NewsService {
     @GET("top-headlines?country=in&apiKey=71a46c4dac0f4761becf0206e7d67f1f")
     fun getTopHeadlines():Single<News>
 }
+//The Single class represents the single value response. Single observable can only emit either a single successful value or an error. It does not emit onComplete event.
