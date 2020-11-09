76be9b4229151f466cfc732273c95cf9e663ad69
Ankit Singh <55789523+singhankit67@users.noreply.github.com>
2020-11-10 02:02:45 +0530

    Update NewsRepository.kt

--- app/src/main/java/com/example/redcarpet/NewsRepository.kt
+++ app/src/main/java/com/example/redcarpet/NewsRepository.kt
@@ -7,9 +7,12 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
+//We have used @Inject above the Repository constructor so that whenever we need a Repository object hilt will provide us Repository Object
 class NewsRepository @Inject constructor(private val newsService : NewsService){
     fun getTopHeadlines():Single<News>{
         return newsService.getTopHeadlines()
             .subscribeOn(Schedulers.io())
+        //this means that we want to subscribe on the background thread
     }
+    
 }
