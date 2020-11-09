b2b7274725a91c5e522e772d32c94a1e52fe385d
Ankit Singh <55789523+singhankit67@users.noreply.github.com>
2020-11-10 02:20:43 +0530

    Update LandingViewModel.kt

--- app/src/main/java/com/example/redcarpet/LandingViewModel.kt
+++ app/src/main/java/com/example/redcarpet/LandingViewModel.kt
@@ -7,14 +7,20 @@ import androidx.lifecycle.ViewModel
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import timber.log.Timber
 
+//Here we see a new annotation @ViewModelInject and it is the best thing about using hilt. If we use dagger in our projects with MVVM
+//we must have faced the problem of injecting viewmodels. The most famous workaround was to create your own ViewModelFactory class but that was a long and tedious work.
+//But with the hilt, this annotation will do all the work and you can easily inject ViewModels.
 class LandingViewModel @ViewModelInject constructor(newsRepository: NewsRepository) : ViewModel() {
-    private val _topHeadlines = MutableLiveData<List<Article1>>()
+    private val _topHeadlines = MutableLiveData<List<Article1>>() //this will update the list
+     //set the topHeadlines(Mutable Live Data) to update the fragment of the changes.
     val topHeadlines: LiveData<List<Article1>>
-        get() = _topHeadlines
+        get() = _topHeadlines //getting the list here in another variable
     init {
         newsRepository.getTopHeadlines()
             .observeOn(AndroidSchedulers.mainThread())
+        //observeOn(AndroidSchedulers.mainThread()) means that we want to observe the data on the main thread
             .subscribe({news -> _topHeadlines.value = news.article},{t -> Timber.e(t)})
+        //a subscription is created here
 
 
     }
