5da621b240d0efd897fe6e181a05b67b16707ea8
Ankit Singh <55789523+singhankit67@users.noreply.github.com>
2020-11-11 09:18:53 +0530

    Update NewsService.kt

--- app/src/main/java/com/example/redcarpet/NewsService.kt
+++ app/src/main/java/com/example/redcarpet/NewsService.kt
@@ -3,6 +3,7 @@ package com.example.redcarpet
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 
+//obeservable
 interface NewsService {
     @GET("top-headlines?country=in&apiKey=71a46c4dac0f4761becf0206e7d67f1f")
     fun getTopHeadlines():Single<News>
